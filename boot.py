dfce6577c28e0071744488d6b2ed2d36ae685eb3
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2022-10-11 23:59:49 -0400

    added some logic to boot:

--- boot.py
+++ boot.py
@@ -1,8 +1,8 @@
 from errno import EALREADY
 from board import GP11
 from digitalio import DigitalInOut, Direction, Pull
-from storage import disable_usb_drive
-from usb_cdc import disable
+from storage import disable_usb_drive, enable_usb_drive
+from usb_cdc import disable, enable
 
 enable_pin = GP11
 enable_switch = DigitalInOut(enable_pin)
@@ -10,6 +10,9 @@ enable_switch.direction = Direction.INPUT
 enable_switch.pull = Pull.UP
 
 if enable_switch.value:
+    enable_usb_drive()
+    enable(console=True, data=False)
+else:
     disable_usb_drive()
     disable()
 
